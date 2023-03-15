7de9a976aa4d6779a7abfce5124dc36f4c8f65f6
elliotsackman <elliotsackman@gmail.com>
2023-03-15 14:56:05 -0400

    Added search method functionality that ties back to frontend widget

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import requests
+import wikipedia
 
 
 Builder.load_file('frontend.kv')
@@ -9,7 +11,19 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        pass
+        # Get user query from text input
+        user_query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and list of image urls
+        page = wikipedia.page(user_query)
+        image_links = page.images
+        # Download the image
+        link = image_links[0]
+        req = requests.get(link)
+        image_path = 'files/image.jpg'
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+        # Set the image in the image widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
