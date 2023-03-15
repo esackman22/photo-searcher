6891a8303692a6780ffd62cd28d5b8021589dc95
elliotsackman <elliotsackman@gmail.com>
2023-03-15 14:22:25 -0400

    Configured initial FirstScreen widgets using frontend.kv

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
@@ -16,5 +20,6 @@ class MainApp(App):
 
     def build(self):
         return RootWidget()
+
 
 MainApp().run()
